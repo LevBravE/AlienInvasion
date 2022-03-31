e57ed27e99bbdb68f0ff9f304a0ca3981b34c0e9
levbrave <lev.brave@gmail.com>
2022-03-31 19:02:24 +0300

    Add Ship and bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,7 @@
 from settings import Settings
+from ship import Ship
+from bullet import Bullet
+
 import sys
 import pygame as pg
 
@@ -10,21 +13,67 @@ class AlienInvasion:
         pg.init()
         self.settings = Settings()
 
-        self.screen = pg.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pg.display.set_caption('Alien Invasion')
 
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
+        self.bullets = pg.sprite.Group()
 
     def run_game(self):
         while True:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+            elif event.type == pg.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pg.KEYUP:
+                self._check_keyup_event(event)
 
-            pg.display.flip()
+    def _check_keydown_events(self, event):
+        if event.key == pg.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pg.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pg.K_q:
+            sys.exit()
+        elif event.key == pg.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_event(self, event):
+        if event.key == pg.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pg.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Обновлении позиции снарядов и удаление старых снарядов"""
+        self.bullets.update()
+
+        # Удаление снарядов вышедших за экран
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        pg.display.flip()
 
 
 if __name__ == '__main__':
